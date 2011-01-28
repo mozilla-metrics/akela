37e898a694b63fc49611dbb0bf8a084bb23d6e0c
Xavier <xavier.stevens@gmail.com>
2011-01-28 10:16:35 -0800

    Adding Size and regex FindAll funcs for pig Adding lsr_diff.py since its part of the backup process

---
+++ src/java/com/mozilla/pig/eval/Size.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2010 Mozilla Foundation
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mozilla.pig.eval;
+
+import java.io.IOException;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.DataBag;
+import org.apache.pig.data.DefaultTuple;
+import org.apache.pig.data.Tuple;
+
+public class Size extends EvalFunc<Long> {
+
+	public Long exec(Tuple input) throws IOException {
+		if (input == null) {
+			return null;
+		}
+		
+		long n = 0;
+		if (input.size() != 0) { 			
+			Object obj = input.get(0);
+			if (obj instanceof DefaultTuple) {
+				n = ((DefaultTuple)obj).size();
+			} else if (obj instanceof DataBag) {
+				n = ((DataBag) obj).size();
+			}
+		}
+		
+		return n;
+	}
+	
+}
