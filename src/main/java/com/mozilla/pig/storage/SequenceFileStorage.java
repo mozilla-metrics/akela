b9bac0a8f73d9d3a7381ff240c6de21f9a95667a
Xavier <xavier.stevens@gmail.com>
2011-12-09 11:00:38 -0800

    Sync'ing changes

--- src/main/java/com/mozilla/pig/storage/SequenceFileStorage.java
+++ src/main/java/com/mozilla/pig/storage/SequenceFileStorage.java
@@ -1,19 +1,41 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.mozilla.pig.storage;
 
 import java.io.IOException;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.SequenceFile.CompressionType;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.OutputFormat;
 import org.apache.hadoop.mapreduce.RecordWriter;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.log4j.Logger;
 import org.apache.pig.StoreFunc;
 import org.apache.pig.data.Tuple;
 
 public class SequenceFileStorage extends StoreFunc {
 
+    private static final Logger LOG = Logger.getLogger(SequenceFileStorage.class);
+    
     @SuppressWarnings("rawtypes")
     protected RecordWriter writer = null;
     
@@ -44,8 +66,7 @@ public class SequenceFileStorage extends StoreFunc {
         try {
             writer.write(outputKey, outputValue);
         } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            LOG.error("Interuppted during write", e);
         }
     }
 
@@ -54,6 +75,8 @@ public class SequenceFileStorage extends StoreFunc {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         FileOutputFormat.setOutputPath(job, new Path(location));
+        FileOutputFormat.setCompressOutput(job, true);
+        SequenceFileOutputFormat.setOutputCompressionType(job, CompressionType.BLOCK);
     }
     
 }
